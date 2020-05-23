b61de61d608b69776b0249f32e3f0133cffea3a9
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-05-23 20:10:08 +0900

    classとinstance

--- instance.php
+++ instance.php
@@ -1,14 +1,51 @@
 <?php
 
 class Product{
-    //アクセス修飾子： privete, protected, public
-
+    //アクセス修飾子： 
+    //privete（クラス内だけ）
+    //protected（自分と継承したクラスのみ）
+    //public（公開）
 
     //変数
-
+    private $product = [];
 
     //関数
+    function __construct($product){
+        $this->product = $product;
+    }
+
+    public function getProduct(){
+        echo $this->product;
+    }
+
+    public function addProduct($item){
+        $this->product .= $item;
+    }
     
+    //staticと付けることで静的に関数を使うことができる
+    public static function getStaticProduct($str){
+        echo $str;
+    }
 }
+
+    $instance = new Product('テスト');
+
+    var_dump($instance);
+
+    $instance->getProduct();
+    echo '<br>';
+
+    $instance->addProduct('追加分');
+    echo '<br>';
+
+    $instance->getProduct();
+    echo '<br>';
+
+    //静的(static) クラス名::関数名
+    Product::getStaticProduct('静的');
+    echo '<br>';
+    
+
+
 
 ?>
